59eeca2471f89ef60d1397d9375b1152e4e78b1d
G Côte <meltwin158@gmail.com>
2024-11-06 14:02:05 +0100

    Default project

---
+++ src/main/java/io/github/meltwin/authlab/client/Client.java
@@ -0,0 +1,13 @@
+package io.github.meltwin.authlab.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class Client
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from client!" );
+    }
+}
